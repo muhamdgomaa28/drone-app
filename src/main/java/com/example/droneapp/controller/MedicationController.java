961574a8d62017ec22f97575b9b8404f31962a16
muhamdgomaa28 <muhamdgomaa28@gmail.com>
2021-11-29 12:43:47 +0200

    feature | get medications for specific drone

--- src/main/java/com/example/droneapp/controller/MedicationController.java
+++ src/main/java/com/example/droneapp/controller/MedicationController.java
@@ -6,6 +6,7 @@ import com.example.droneapp.service.MedicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,13 @@ public class MedicationController {
     public List<MedicationModel> getAllMedications() {
         return medicationService.getAllMedications();
     }
+
+
+    @GetMapping("/medications/drone")
+    @ResponseStatus(HttpStatus.OK)
+    public List<MedicationModel> getLoadedMedicationItemsByDrone(@RequestParam String serialNumber) {
+        return medicationService.getLoadedMedicationItemsForDrone(serialNumber);
+    }
+
+
 }
--- src/main/java/com/example/droneapp/service/impl/MedicationServiceImpl.java
+++ src/main/java/com/example/droneapp/service/impl/MedicationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.droneapp.service.impl;
 
+import com.example.droneapp.entities.Drone;
 import com.example.droneapp.entities.Medication;
+import com.example.droneapp.exception.BusinessException;
 import com.example.droneapp.model.MedicationModel;
+import com.example.droneapp.repository.DroneRepository;
 import com.example.droneapp.repository.MedicationRepository;
 import com.example.droneapp.service.MedicationService;
 import com.example.droneapp.service.mappers.MedicationMapper;
@@ -22,9 +25,27 @@ public class MedicationServiceImpl implements MedicationService {
     @Autowired
     private MedicationMapper medicationMapper;
 
+    @Autowired
+    private DroneRepository droneRepository;
+
+
+
     @Override
     public List<MedicationModel> getAllMedications() {
         return medicationRepository.findAll().stream().map(medication -> medicationMapper.toModel(medication)).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<MedicationModel> getLoadedMedicationItemsForDrone(String serialNumber) {
+        Drone drone = droneRepository.findBySerialNumber(serialNumber)
+                .orElseThrow(() -> new BusinessException("Could not find Drone with serial Number provided"));
+
+        return drone.getDroneMedications()
+                .stream()
+                .map(droneMedication -> droneMedication.getMedication())
+                .map(medication -> medicationMapper.toModel(medication))
+                .collect(Collectors.toList());
 
     }
 }
